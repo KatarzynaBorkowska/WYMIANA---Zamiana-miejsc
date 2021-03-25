c5d11a55fad919bc3dc1d1d1b8c82c7f4878e11f
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-03-25 22:21:37 +0100

    Initial Commit

---
+++ WYMIANA - Zamiana miejsc/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WYMIANA - Zamiana miejsc
+//
+//  Created by Kasia on 25/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
